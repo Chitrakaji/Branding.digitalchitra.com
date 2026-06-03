bd2e868d6c0e12e194bc457677274e21cbeb1a15
Chitrakaji <gurungchitrakaji@gmail.com>
2026-06-03 14:08:47 +0545

    Add Meta Pixel and update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import type { ReactNode } from "react";
 import { Manrope } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const manrope = Manrope({
@@ -13,9 +14,9 @@ export const metadata: Metadata = {
   description:
     "Book a free 1:1 consultation and get a customized digital marketing growth plan for your Nepal-based business.",
   icons: {
-    icon: "/site_icon_DC1.png",
-    shortcut: "/site_icon_DC1.png",
-    apple: "/site_icon_DC1.png",
+    icon: "/site_icon_DC1.jpg",
+    shortcut: "/site_icon_DC1.jpg",
+    apple: "/site_icon_DC1.jpg",
   },
   keywords: [
     "digital marketing consultant Nepal",
@@ -47,7 +48,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={manrope.variable} suppressHydrationWarning>
-      <body suppressHydrationWarning>{children}</body>
+      <body suppressHydrationWarning>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1671284467546694');
+fbq('track', 'PageView');`}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1671284467546694&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {children}
+      </body>
     </html>
   );
 }
